52dbe2581e67346d700d36589586b60c5781efe4
jimeeam <139251508+jimeeam@users.noreply.github.com>
2023-07-18 09:34:39 -0600

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //ciclo while// //Andrea//
+using namespace std;
+int main () {int numero;
+  cout<<"Ingrese un valor";
+  cin>>numero;
+  while (numero <=100)
+  {
+      cout<<"Ingrese un valor";
+      cin>> numero;
+ }
+ return 0;
+}
